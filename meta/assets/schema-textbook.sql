51761ffa75a65d534b846e316c3a3319ad2fcb8f
Jason A. Clark <jasclark@gmail.com>
2015-12-29 00:49:42 -0700

    removed AUTO_INCREMENT value from CREATE TABLE statement

--- meta/assets/schema-textbook.sql
+++ meta/assets/schema-textbook.sql
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `bodymatter` (
   `inLanguage` varchar(5) DEFAULT NULL,
   PRIMARY KEY (`id`),
   FULLTEXT KEY `search` (`name`,`creator`,`description`,`genre`,`keywords`,`dateCreated`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=64 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `bodymatter`
